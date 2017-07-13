067a2466534fc4ad6d14ff1a98e5c0bc568b90e3
Oula P <oula.piisalo@gmail.com>
2017-07-13 19:38:08 +0300

    Added functionality to add reservations to database

--- server.js
+++ server.js
@@ -1,19 +1,47 @@
-const HTTP = require("http");
-const EXPRESS = require("express");
-const MONGO = require("mongodb").MongoClient;
+const HTTP        = require("http");
+const EXPRESS     = require("express");
+const MONGO       = require("mongodb").MongoClient;
+const ASSERT      = require("assert");
+const BODY_PARSER = require('body-parser');
+const MULTER      = require('multer');
 
-MONGO.connect("mongodb://localhost:27017", function(err, db) {
+const URL = 'mongodb://localhost:27017/tiva';
+
+MONGO.connect(URL, function(err, db) {
   if(err) {
     return console.dir(err);
   }
 });
 
 var app = EXPRESS();
-app.use(EXPRESS.static(__dirname + "/public"));
+var router = EXPRESS.Router();
+var upload = MULTER(); // for parsing multipart/form-data
+
+app.use(EXPRESS.static("public"));
+app.use(BODY_PARSER.json());
+app.use(BODY_PARSER.urlencoded({ extended: true }));
 
 app.get('/', function(req, res){
   console.log("GET req");
   res.sendFile(__dirname + "/views/index.html");
+});
+
+app.post('/insert-reservation', upload.array(), function(req, res, next){
+  var reservation = {
+    room: req.body.room,
+    time_start: req.body.time_start,
+    time_end: req.body.time_end
+  };
+  console.log(req.body);
+  MONGO.connect(URL, function(err, db) {
+    ASSERT.equal(null, err);
+    db.collection("myCollection").insertOne(reservation,  function(err, result){
+      ASSERT.equal(null, err);
+      console.log("Reservation inserted");
+      db.close();
+    });
+  });
+  res.redirect('/');
 });
 
 app.listen(8080, function() {
