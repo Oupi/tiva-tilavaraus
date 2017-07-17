438ddec0eeb5679c3d525730768dd482de4c803a
Aleksi Ahvamaa <aleksi.ahvamaa@gmail.com>
2017-07-17 14:39:55 +0300

    Added little more data and minor fixes to tests.

--- test/reservations_test.js
+++ test/reservations_test.js
@@ -5,19 +5,30 @@ const Reservation = require('../models/reservation');
 // Describe tests
 describe('Tests for reservations', function(){
 
-  var reservation;
+  var reservation01 = new Reservation({
+    room: "Saunatila",
+    time_start: new Date("2017-07-23T18:00"),
+    time_end: new Date("2017-07-23T19:00")
+  });
+
+  var reservation02 = new Reservation({
+    room: "Saunatila",
+    time_start: new Date("2017-07-23T19:15"),
+    time_end: new Date("2017-07-23T20:00")
+  });
+
+  var reservation03 = new Reservation({
+    room: "Saunatila",
+    time_start: new Date("2017-07-23T20:15"),
+    time_end: new Date("2017-07-23T21:00")
+  });
+
+  var reservations = [reservation01, reservation02, reservation03];
+
   // Before each test we insert data to reservations collection
   beforeEach(function(done){
-    // Create a new reservation from the reservation model,
-    // so that there is data in the db.
-      reservation = new Reservation({
-      room: "Saunatila",
-      time_start: new Date("2017-07-23T18:00"),
-      time_end: new Date("2017-07-23T19:00")
-    });
-    // Save the reservation to the database
-    // save() is asynchronous
-    reservation.save().then(function(){
+    // Save the reservations to the database
+    Reservation.insertMany(reservations).then(function(err, docs){
       // Tell mocha that the test is done and move on to the tests
       done();
     });
@@ -32,20 +43,25 @@ describe('Tests for reservations', function(){
     reservationToSave.save().then(function(){
       // Check if the reservation has been saved
       assert(reservationToSave.isNew === false);
-      done();
-    });
+
+      // Find all the reservations and check number of them
+      Reservation.find({}).then(function(result){
+        assert(result.length === 4);
+        done();
+      });
   });
+});
 
   it('Finds a reservation by id from database', function(done){
-    Reservation.findOne({_id: reservation._id}).then(function(result){
-      assert(result._id.toString() === result._id.toString());
+    Reservation.findOne({_id: reservations[0]._id}).then(function(result){
+      assert(result._id.toString() === reservations[0]._id.toString());
       done();
     });
   });
 
   it('Deletes a reservation from database by id', function(done){
-    Reservation.findOneAndRemove({_id: reservation._id}).then(function(){
-      Reservation.findOne({_id: reservation._id}).then(function(result){
+    Reservation.findOneAndRemove({_id: reservations[1]._id}).then(function(){
+      Reservation.findOne({_id: reservations[1]._id}).then(function(result){
         assert(result === null);
         done();
       });
@@ -53,10 +69,10 @@ describe('Tests for reservations', function(){
   });
 
   it('updates a record in database', function(done){
-    Reservation.findOneAndUpdate({_id: reservation._id}, {time_start: new Date("2017-07-23T19:00"), time_end: new Date("2017-07-23T20:00")}).then(function(){
-      Reservation.findOne({_id: reservation._id}).then(function(result){
-        assert(result.time_start !== reservation.time_start);
-        assert(result.time_end !== reservation.time_end);
+    Reservation.findOneAndUpdate({_id: reservations[2]._id}, {time_start: new Date("2017-07-23T20:45"), time_end: new Date("2017-07-23T22:00")}).then(function(){
+      Reservation.findOne({_id: reservations[2]._id}).then(function(result){
+        assert(result.time_start !== reservations[2].time_start);
+        assert(result.time_end !== reservations[2].time_end);
         done();
       });
     });
