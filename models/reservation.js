e7150088ffd3232feef44716b3a56daf85fbcee1
Aleksi Ahvamaa <aleksi.ahvamaa@gmail.com>
2017-07-17 15:21:14 +0300

    Updated reservations schema/model accordingly to specifications. Also updated data types to documentation.

--- models/reservation.js
+++ models/reservation.js
@@ -1,12 +1,21 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
 
 // Create Schema and Model
 // Schema with expected fields and datatypes
 const ReservationSchema = new Schema({
+  room_id: ObjectId,
   room: String,
-  time_start: Date,
-  time_end: Date
+  user: {
+    userId: ObjectId,
+    name: String,
+    email: String,
+    phonenumber: String
+  },
+  time_start: String,
+  time_end: String,
+  time_cancel: String
 });
 
 // Every time new Reservation is made, it is made to reservation(s) collection
