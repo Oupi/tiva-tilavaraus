93cf2c598170b9636caa6171489a0f96ee5fb5a9
Ditsur <ditsur@gmail.com>
2017-07-24 18:19:59 +0300

    Initial controller.js and factories.js files

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,1 +1,69 @@
 var app = angular.module('Controllers', ['Factories']);
+
+// UserController
+app.controller('UserController', function($scope, UserFactory){
+	
+	$scope.login = function(userName, password){
+		UserFactory.login(userName, password)
+			.then(function(data) {
+			
+			},function(reason) {
+				
+			});
+	}
+	
+	$scope.register = function(){
+		
+	}
+	
+	$scope.editUser = function(){
+		
+	}
+	
+	$scope.deleteUser = function(userId){
+		
+	}
+	
+});
+
+// ReservationController
+app.controller('ReservationController', function($scope, ReservationFactory){
+	
+	$scope.makeReservation = function(userId, roomId, starTime, endTime){
+		
+	}
+	
+	$scope.findReservationsByRoom = function(roomId){
+		
+	}
+	
+	$scope.findReservationsByUser = function(userId){
+		
+	}
+	
+	$scope.findReservationsByTime = function(startTime, endTime){
+		
+	}
+	
+	$scope.removeReservation = function(reservationId){
+		
+	}
+	
+});
+
+// RoomController
+app.controller('RoomController', function($scope, RoomFactory){
+	
+	$scope.createRoom = function(){
+		
+	}
+	
+	$scope.editRoom = function(){
+		
+	}
+	
+	$scope.deleteRoom = function(){
+		
+	}
+	
+});
--- public/js/factories.js
+++ public/js/factories.js
@@ -1,1 +1,86 @@
 var app = angular.module('Factories', []);
+
+// UserFactory
+app.factory('UserFactory', function($http) {
+	
+	var factory = {};
+	
+	factory.login = function(userName, password) {
+    return $http({
+        method:"POST",
+        url:"login",
+        data:{"userName":userName,"password":password},
+        headers:{"Content-Type":"application/json"}
+    });
+  }
+	
+  factory.register = function(userName, password) {
+    return $http({
+        method:"POST",
+        url:"register",
+        data:{"userName":userName,"password":password},
+        headers:{"Content-Type":"application/json"}
+    });
+  }
+	
+	factory.editUser = function() {
+	
+	}
+	
+	factory.deleteUser = function() {
+	
+	}
+	
+	return factory;
+	
+});
+
+// ReservationFactory
+app.factory('ReservationFactory', function($http) {
+	
+	var factory = {};
+	
+	factory.makeReservation = function(userId, roomId, starTime, endTime){
+		
+	}
+	
+	factory.findReservationsByRoom = function(roomId){
+		
+	}
+	
+	factory.findReservationsByUser = function(userId){
+		
+	}
+	
+	factory.findReservationsByTime = function(startTime, endTime){
+		
+	}
+	
+	factory.removeReservation = function(reservationId){
+		
+	}
+	
+	return factory;
+	
+});
+
+// RoomFactory
+app.factory('RoomFactory', function($http) {
+	
+	var factory = {};
+	
+	factory.createRoom = function(){
+		
+	}
+	
+	factory.editRoom = function(){
+		
+	}
+	
+	factory.deleteRoom = function(){
+		
+	}
+	
+	return factory;
+	
+});
