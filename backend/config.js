a8ad407d8f87d5e2e40405e19cbcef36d359b976
Aleksi Ahvamaa <aleksi.ahvamaa@gmail.com>
2017-08-01 14:25:26 +0300

    Added possibilities for using cloud databases.
    connection options for cloud dbs are there but in server.js local dbs are in use before settings for choosing enviroments are made

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,17 @@
+var databaseUser = "tivausr";
+var databasePword = "NyPTeMMTKnXqZJFaYQjhceNBudgTGw";
+var sessionDBUser = "sestivausr";
+var sessionDBPword = "nnaMFtAPTkvpWhYwbTnCTrDSCpahNS";
+
+/* TODO session datahandling and configuration for choosing enviroment
+** example production or development/testing
+*/
 module.exports = {
-  "database":"mongodb://localhost/tiva",
+  "database": "mongodb://localhost/tiva-test",
+  "databaseCloud": "mongodb://" + databaseUser + ":" + databasePword + "@ds129733.mlab.com:29733/tiva",
   "testDB":"mongodb://localhost/tiva-test",
   "sessionDB":"mongodb://localhost/tiva-session",
+  "sessionDBCloud":"mongodb://"+ sessionDBUser + ":" + sessionDBPword + "@ds047504.mlab.com:47504/tiva-session",
   "secret":"victoryisboring",
   "saltRounds":10
 };
